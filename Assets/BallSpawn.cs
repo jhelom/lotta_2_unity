e731fe3f4333c70c4e50ab3734895e7d213f5ea1
jhelom <1127424+jhelom@users.noreply.github.com>
2024-08-20 12:26:30 +0900

    砲台上下移動

--- Assets/BallSpawn.cs
+++ Assets/BallSpawn.cs
@@ -5,9 +5,15 @@ using Unity.VisualScripting;
 using UnityEngine;
 public class BallSpawn : MonoBehaviour
 {
-    public float minAngle = 30.0f;
-    public float maxAngle = 90.0f;
-    public float spawnInterval = 0.1f;
+    public float upDownAmplitude = 1.0f; // 振幅（上下の移動距離）
+    public float upDownSpeed = 1.0f; // 移動速度
+
+    private float upDownStartY;
+    public float rotationMin = 30.0f;
+    public float rotationMax = 90.0f;
+    public float shootInterval = 0.1f;
+
+    public int shootCount = 1;
 
 
     public int shootSpeed = 10;
@@ -19,6 +25,7 @@ public class BallSpawn : MonoBehaviour
     void Start()
     {
         prefab = Resources.Load("Ball") as GameObject;
+        upDownStartY = transform.position.y;
         StartCoroutine(RepeatAction());
     }
 
@@ -26,15 +33,25 @@ public class BallSpawn : MonoBehaviour
     void Update()
     {
         Rotate();
+        UpDown();
     }
 
     IEnumerator RepeatAction()
     {
         while (true)
         {
-            yield return new WaitForSeconds(spawnInterval);
-            SpawnBall();
+            yield return new WaitForSeconds(shootInterval);
+            for (var i = 0; i < shootCount; i++)
+            {
+                SpawnBall(i);
+            }
         }
+    }
+
+    private void UpDown()
+    {
+        var y = upDownStartY + upDownAmplitude * Mathf.Cos(Time.time * upDownSpeed);
+        transform.position = new Vector3(transform.position.x, y, transform.position.z);
     }
 
     private void Rotate()
@@ -46,21 +63,18 @@ public class BallSpawn : MonoBehaviour
         float t = (cosValue + 1) / 2;
 
         // 角度の補間
-        float angle = Mathf.Lerp(minAngle, maxAngle, t);
+        float angle = Mathf.Lerp(rotationMin, rotationMax, t);
 
         // Z軸周りの回転を適用
         transform.rotation = Quaternion.Euler(0, 0, angle);
 
     }
-    private void SpawnBall()
+    private void SpawnBall(int i)
     {
-
-        // 発射方向を計算
-        // var rotation = Quaternion.Euler(0, 0, transform.rotation.z);
-        // var shootDirection = rotation * Vector2.right;
         var pos = new Vector2(transform.position.x, transform.position.y);
         var ball = Instantiate(prefab, pos, transform.rotation);
         var rb = ball.GetComponent<Rigidbody2D>();
-        rb.velocity = transform.right * shootSpeed;
+        var r = i == 0 ? 1 : Random.Range(0.5f, 1.5f);
+        rb.velocity = transform.right * (shootSpeed * r);
     }
 }
